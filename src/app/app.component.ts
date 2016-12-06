b4bc9a7b2df8bb1929ac54fdb23d7e9a338e0511
abennehhou <a.bennehhou@arkadin.com>
2016-12-06 13:16:35 +0100

    angular tour of heroes - stop 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { Hero } from './common/domain/hero'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Tour of Heroes';
+
+  hero: Hero = {
+    id: 123,
+    name: 'Windstorm'
+  };
 }
